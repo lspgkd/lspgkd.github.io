c1a8d9b42f902cd881173cf072b59dff7404d808
lspgkd <73596317+lspgkd@users.noreply.github.com>
2022-01-23 23:51:59 +0800

    修改

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '小邓无语';
+    if (score <= 99) return '小邓觉得一般';
+    if (score <= 149) return '小邓觉得你很棒';
+    if (score <= 199) return '小邓惊呆了';
     return '人？';
 }
 
